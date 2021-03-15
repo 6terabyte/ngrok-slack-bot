34a7466d5067d55b678ff3c81a7ec5e40de3750c
rinkei <6terabyte.onlyreceive@gmail.com>
2021-03-15 23:28:21 +0900

    :sparkles: for new server

--- index.ts
+++ index.ts
@@ -8,17 +8,38 @@ import ngrok from 'ngrok';
 import isOnline from 'is-online';
 import delay from 'delay';
 import { IncomingWebhook } from '@slack/webhook';
-import ip from 'ip';
+//import ip from 'ip';
+import { exec, execSync } from 'child_process';
 
 const webhook = new IncomingWebhook(ENV.SLACK_WEBHOOK_URL);
+
+const getGlobalIP = () => {
+  return String(execSync('curl -s https://checkip.amazonaws.com')).trim();
+};
 
 (async () => {
   while (!(await isOnline())) {
     await delay(3000);
   }
-  await ngrok.authtoken(ENV.NGROK_TOKEN);
-  const ngrokUrl = await ngrok.connect({ proto: 'tcp', addr: ENV.SSH_PORT });
-  await webhook.send({
-    text: `server restarted: ${ngrokUrl}\nip address: ${ip.address()}`,
-  });
+  if ((ENV.X11VNC || '').toLowerCase() === 'true') {
+    exec('x11vnc -forever -display :0 -rfbauth ~/.x11vnc/passwd -localhost');
+  }
+  let restartMsg = 'server restarted:\n';
+  let address = getGlobalIP(); //ip.address();
+  restartMsg += `ip: ${address}\n`;
+  if (ENV.NGROK_TOKEN) {
+    await ngrok.authtoken(ENV.NGROK_TOKEN);
+    restartMsg += `ngrok: ${await ngrok.connect({
+      proto: 'tcp',
+      addr: ENV.SSH_PORT,
+    })}\n`;
+  }
+  await webhook.send({ text: restartMsg });
+
+  setInterval(async () => {
+    if (getGlobalIP() != address) {
+      address = getGlobalIP();
+      await webhook.send({ text: `ip changed: ${address}` });
+    }
+  }, 5000);
 })();
